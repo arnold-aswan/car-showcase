4409e2121c22741639fcd19d49296f2217567113
arnold-aswan <fredyaswan@gmail.com>
2023-12-11 13:38:36 +0300

    removed unused code

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,5 @@
 import { CarProps, FilterProps } from "@/types";
 
-
 export const fetchCars = async (filters: FilterProps) => {
   const { manufacturer, year, model, fuel, limit } = filters;
   const headers = {
@@ -36,12 +35,11 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
 
-  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("customer", process.env.CAR_IMAGE_API_KEY);
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
   url.searchParams.append("modelYear", `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
   url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
